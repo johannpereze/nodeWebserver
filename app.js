b9465054afc481170e5b7fbdba032e34fbeb14b9
johannpereze <sebas.jsp@gmail.com>
2021-09-22 21:46:15 -0500

    Sirviendo una web estática

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 
 // servir contenido estático
-app.use(express.static('public'))
- 
+app.use(express.static("public"));
 
-app.get('/hola-mundo', (req, res)=> {
-  res.send('Hello World from its route')
-})
-app.get('*', (req, res)=> {
-  res.send('404 | Not found')
-})
- 
-app.listen(port,()=>{
-    console.log(`This app is listening at http://localhost:${port}`);
-})
+app.get("/hola-mundo", (req, res) => {
+  res.send("Hello World from its route");
+});
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+app.listen(port, () => {
+  console.log(`This app is listening at http://localhost:${port}`);
+});
