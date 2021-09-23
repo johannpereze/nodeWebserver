0a98c0963cc39cf1bdf0083e1869a0e7c24d3e11
johannpereze <sebas.jsp@gmail.com>
2021-09-22 21:29:21 -0500

    Montado el servidor de express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+// servir contenido estático
+app.use(express.static('public'))
+ 
+app.get('/', (req, res)=> {
+  res.send('Hello World')
+})
+app.get('/hola-mundo', (req, res)=> {
+  res.send('Hello World from its route')
+})
+app.get('*', (req, res)=> {
+  res.send('404 | Not found')
+})
+ 
+app.listen(port,()=>{
+    console.log(`This app is listening at http://localhost:${port}`);
+})
