88bb210fdff5bf6365fdb99ce3468b201af96545
johannpereze <sebas.jsp@gmail.com>
2021-09-22 22:14:34 -0500

    instalamos handelbars hbs para manejar templates en express

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const port = 8080;
 // servir contenido estático
 app.use(express.static("public"));
 
-app.get("/hola-mundo", (req, res) => {
-  res.send("Hello World from its route");
+app.get("/generic", (req, res) => {
+  res.sendFile(__dirname + "/public/generic.html");
+});
+app.get("/elements", (req, res) => {
+  res.sendFile(__dirname + "/public/elements.html");
 });
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/public/404.html");
