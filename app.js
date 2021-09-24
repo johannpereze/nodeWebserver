8b1e5b7d6a7fc0d397c51b7ee1cb14ddb3e02c17
johannpereze <sebas.jsp@gmail.com>
2021-09-23 21:35:58 -0500

    Preparadas variables de entorno y script de start para producción

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const express = require("express");
-const app = express();
-const port = 8080;
+const hbs = require("hbs");
+require('dotenv').config()
 
+const app = express();
+const port = process.env.PORT;
+
+//Handelbars
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 // servir contenido estático
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    nombre: "Sebastian Pérez",
+    titulo: "Curso de Node",
+  });
 });
 app.get("/generic", (req, res) => {
-  res.sendFile(__dirname + "/public/generic.html");
+  res.render("generic", {
+    nombre: "Sebastian Pérez",
+    titulo: "Curso de Node",
+  });
 });
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname + "/public/elements.html");
+  res.render("elements", {
+    nombre: "Sebastian Pérez",
+    titulo: "Curso de Node",
+  });
 });
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(__dirname + "/public/404.html");
+// });
 
 app.listen(port, () => {
   console.log(`This app is listening at http://localhost:${port}`);
